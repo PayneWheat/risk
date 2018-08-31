962ffc65b416731bc0af0abfbf7631c51f966261
pasieng <42784814+pasieng@users.noreply.github.com>
2018-08-30 22:02:02 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -7,12 +7,12 @@ public class Player
 	int countriesOccupied;
 	int continentsOccupied;
 	
-	Player(String playerNum, int armies, int diceValue, int countriesOccupied, int continentsOccupied)
+	Player(String playerNum, int armies, int diceValue, int territoriesOccupied, int continentsOccupied)
 	{
 		this.playerNum = playerNum;
 		this.armies = armies;
 		this.diceValue = diceValue;
-		this.countriesOccupied = countriesOccupied;
+		this.territoriesOccupied = countriesOccupied;
 		this.continentsOccupied = continentsOccupied;
 		
 	}
