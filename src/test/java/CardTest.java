d0a632f60a92e8dc4a9552cae351338467f8ad1e
Payne <wheatp5@gmail.com>
2018-09-19 20:33:20 -0500

    Test template

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,11 @@
+package test.java;
+import junit.framework.TestCase;
+
+public class CardTest extends TestCase {
+	public CardTest(String name) {
+		super(name);
+	}
+	public void testGetCardName() throws Exception {
+		assertTrue(true);
+	}
+}
