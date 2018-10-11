7459fa14e4614b49cb35f7cb52ed35793bbd651e
pasieng <42784814+pasieng@users.noreply.github.com>
2018-10-10 22:15:01 -0500

    Update Player.java

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -4,77 +4,31 @@ import java.util.*;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class Player{
+public class Player 
+{
 	String name;
 	String color;
 	int armies;
-	int diceValue;
-	int territoriesOccupied;
-	int countriesOccupied;
-	int continentsOccupied;
 	ArrayList<Card> cards;
 	int currency;
 	int credits;
-	String attackMessage;
-	private List<Observer> observers = new ArrayList<>();
-	
-	Player() {
+	public Player() {
 		this.name = "";
 		this.color = "";
 		this.armies = 0;
-		this.diceValue = 0;
-		this.territoriesOccupied = 0;
-		this.continentsOccupied = 0;
-		this.countriesOccupied = 0;
 		this.cards = new ArrayList<Card>();
-		currency = 0;
+		this.currency = 0;
 		this.credits = 0;
-	}
-	public Player(String name, String color) {
-		this.name = name;
-		this.color = color;
-		this.armies = 0;
-		this.diceValue = 0;
-		this.territoriesOccupied = 0;
-		this.countriesOccupied = 0;
-		this.continentsOccupied = 0;
-		this.cards = new ArrayList<Card>();
-	}
-	public Player(String name, String color, int armies, int diceValue, int territoriesOccupied, int continentsOccupied){
-
-		this.name = name;
-		this.color = color;
-		this.armies = armies;
-		this.diceValue = diceValue;
-		this.territoriesOccupied = countriesOccupied;
-		this.continentsOccupied = continentsOccupied;	
-		this.cards = new ArrayList<Card>();
 	}
 	public Player(String name, String color, int currency, int credits) {
 		this.name = name;
 		this.color = color;
 		this.armies = 0;
-		this.diceValue = 0;
-		this.territoriesOccupied = 0;
-		this.countriesOccupied = 0;
-		this.continentsOccupied = 0;
 		this.cards = new ArrayList<Card>();
 		this.currency = currency;
 		this.credits = credits;
 	}
-	public Player(String name, String color, int armies, int diceValue, int territoriesOccupied, int continentsOccupied, int currency, int credits){
 
-		this.name = name;
-		this.color = color;
-		this.armies = armies;
-		this.diceValue = diceValue;
-		this.territoriesOccupied = countriesOccupied;
-		this.continentsOccupied = continentsOccupied;	
-		this.cards = new ArrayList<Card>();
-		this.currency = currency;
-		this.credits = credits;
-	}
-	
 	public String getName() {
 		return name;
 	}
@@ -83,15 +37,6 @@ public class Player{
 	}
 	public int getArmies(){
 		return armies;
-	}
-	public int getDiceValue(){
-		return diceValue;
-	}
-	public int getTerritoriesOccupied(){
-		return territoriesOccupied;
-	}
-	public int continentsOccupied(){
-		return continentsOccupied;
 	}
 	public int getCurrency(){
 		return currency;
@@ -119,21 +64,6 @@ public class Player{
 	public void useCredits(int credits){
 		this.credits = this.credits - credits;
 	}
-	public void addObserver(Observer observer) {
-        observers.add(observer);
-    }
-    public void removeObserver(Observer observer) {
-        observers.remove(observer);
-    }
-    public void setAttackMessage(Player p, String attackMessage) {
-        this.attackMessage = attackMessage;
-        for (Observer observer : observers) {
-            observer.update(p, this.attackMessage);
-        }
-    }
-	public String getAttackMessage(){
-		return attackMessage;
-	}
 	
 	/**
 	 * 
@@ -150,24 +80,30 @@ public class Player{
 		else
 			territoryIndex = JOptionPane.showInputDialog(getName() + ", input a territory index to place at least one army (" + getArmies() + " remaining)");
 		
-		// TODO: Abstract out a function for parsing input from JOptionPane 
-		try {
-			ti = Integer.parseInt(territoryIndex);
-		} catch(NumberFormatException e) {
-			// not an int
-			System.out.println("Could not parse number. Try again");
-			ti = pickTerritory(initialTurns);
-		} catch(Exception e) {
-			// not a territory index
-			System.out.println("Error: " + e + "\nTry again");
+		ti = numberInputParser(territoryIndex);
+		if(ti < 0) {
 			ti = pickTerritory(initialTurns);
 		}
-			
-		
 		return ti;
 	}
 	
-	//TODO: move to Player class
+	public int numberInputParser(String input) {
+		int number = -1;
+		try {
+			number = Integer.parseInt(input);
+			if(number < 0) {
+				number = -1;
+				throw new IllegalArgumentException("Must be a positive number.");
+			}
+		} catch(NumberFormatException e) {
+			// not an int
+			System.out.println("Could not parse number. Try again");
+		} catch(Exception e) {
+			System.out.println("Error: " + e + "\nTry again");
+		}
+		return number;
+	}
+	
 	/**
 	 * Prompts user to choose how many armies to place on a territory.
 	 * @return number of armies to be placed
@@ -192,19 +128,8 @@ public class Player{
 			else if(n == JOptionPane.YES_OPTION){
 				undo = false;
 			}
-			try {
-				armies = Integer.parseInt(armyQty);
-			} catch(NumberFormatException e) {
-				// not an int
-				System.out.println("Could not parse number. Try again");
-				armies = chooseArmiesQty();
-				undo = true;
-			} catch(Exception e) {
-				// not a territory index
-				System.out.println("Error: " + e + "\nTry again");
-				armies = chooseArmiesQty();
-				undo = true;
-			}
+			armies = numberInputParser(armyQty);
+
 			if(armies > getArmies()) {
 				System.out.println("You don't have that many armies.");
 				armies = chooseArmiesQty();
@@ -240,29 +165,15 @@ public class Player{
 			}
 		}
 		String attackingTerritoryInput = JOptionPane.showInputDialog(getName() + ", choose a territory to attack from.");
-		int attackingTerritoryIndex = -1;
+		int attackingTerritoryIndex = numberInputParser(attackingTerritoryInput);
 		Territory tempTerritory = new Territory();
-		// TODO: abstract out a function to parse JOptionPane input
-		try {
-			attackingTerritoryIndex = Integer.parseInt(attackingTerritoryInput);
+		if(attackingTerritoryIndex < 0)
+			tempTerritory = chooseAttackingTerritory(allTerritories, territories);
+		else
 			tempTerritory = territories.get(attackingTerritoryIndex);
-		} catch(NumberFormatException e) {
-			// not an int
-			System.out.println("Could not parse number. Try again");
-			tempTerritory = chooseAttackingTerritory(allTerritories, territories);
-		} catch(Exception e) {
-			System.out.println("Error: " + e + "\nTry again");
-			tempTerritory = chooseAttackingTerritory(allTerritories, territories);
-		}
-		/*
-		// TODO: Change these to try/catch blocks and throw proper exceptions
-		if(tempTerritory.getPlayer() != this) {
-			System.out.println("You do not occupy selected territory. Try again");
-			attackingTerritoryIndex = chooseAttackingTerritory(allTerritories, territories);
-		}
-		*/
 		return tempTerritory;
 	}
+	
 	/**
 	 * Prompts player to choose a territory to attack.
 	 * For use when the player is in the attack step of their turn.
@@ -279,19 +190,12 @@ public class Player{
 			System.out.println("[" + tempIndex + "]" + opposingAdjacents.get(i).getTerritoryName() + " (" + opposingAdjacents.get(i).getPlayer().getName()  + "): " + opposingAdjacents.get(i).getArmyCount() + " armies.");
 		}
 		String defendingTerritoryInput = JOptionPane.showInputDialog(getName() + ", choose an opponent's territory to attack.");
-		int defendingTerritoryIndex = -1;
-		try {
-			defendingTerritoryIndex = Integer.parseInt(defendingTerritoryInput);
-			tempTerritory = territories.get(defendingTerritoryIndex);
-		} catch(NumberFormatException e) {
-			// not an int
-			System.out.println("Could not parse number. Try again");
-			tempTerritory = chooseTerritoryToAttack(attackingTerritory, territories);
-		} catch(Exception e) {
-			// not a territory index
-			System.out.println("Error: " + e + "\nTry again");
+		int defendingTerritoryIndex = numberInputParser(defendingTerritoryInput);
+		if(defendingTerritoryIndex < 0) {
+			System.out.println("Try again.");
 			tempTerritory = chooseTerritoryToAttack(attackingTerritory, territories);
 		}
+		tempTerritory = territories.get(defendingTerritoryIndex);
 		if(tempTerritory.getPlayer() == this) {
 			System.out.println("You cannot attack your own territory. Try again");
 			tempTerritory = chooseTerritoryToAttack(attackingTerritory, territories);
@@ -305,6 +209,12 @@ public class Player{
 		return tempTerritory;
 	}
 	
+	/**
+	 * Checks if this player has a set of risk cards to turn in
+	 * If true, prompt the player which set of cards to turn in,
+	 * also force the player to select a set of cards if they have more than 5 cards total. 
+	 * @return set of cards to be exchanged for bonus armies
+	 */
 	public ArrayList<Card> cardCheck() {
 		for(int i = 0; i < cards.size(); i++) {
 			System.out.println("\t" + cards.get(i).getCardTypeName() + " (" + cards.get(i).getTerritoryName() + ")");
@@ -395,11 +305,6 @@ public class Player{
 					}
 						
 					tryAgain = false;
-					/*
-					int bonusArmies = turnInCardSet(this, CardSets.get(cardSetIndex));
-					System.out.println(getName() + " turned in a set of Risk Cards and was awarded " + bonusArmies + " armies.");
-					increaseArmies(bonusArmies);
-					*/
 				} catch(NumberFormatException e) {
 					// not an int
 					System.out.println("Could not parse number. Try again");
@@ -413,7 +318,7 @@ public class Player{
 	/**
 	 * Adds all possible subsets of supplied cards to the subsets parameter
 	 * @param organizedCards An ArrayList of cards to check for each unique permutation. 
-	 * In practice, this only needs to be called when the player has more than one possible sets of cards to turn in.
+	 * In practice, this only needs to be called when the player has more than one possible set of cards to turn in.
 	 * Additionally, this only needs to be called when the player has three of a kind or one or more wild cards
 	 * In the case the player has one or more of each card, the oneOfEachExtractor method will be called. 
 	 * @param n organizedCards size
@@ -439,7 +344,7 @@ public class Player{
 		getSubsetsUtil(organizedCards, n, r, index, data, i + 1, subsets);
 	}
 	/**
-	 * If the player has three of a kind, find all the possible subsets of the card type 
+	 * Returns all possible subsets from supplied, organized cards
 	 * @param organizedCards needs to contain only the cards that make up all of the three of a kind subsets
 	 * @return 2D ArrayList of all possible subsets of size 3
 	 */
@@ -450,7 +355,7 @@ public class Player{
 		return subsets;
 	}
 	/**
-	 * Organizes the player's cards to be supplied to the proper subset function
+	 * Organizes the player's cards to be supplied to the threeOfAKindSubsets method
 	 * @param playerCards Current player's cards
 	 * @param infantryCount The count of infantry cards the player is in possession of
 	 * @param cavalryCount The count of cavalry cards the player is in possession of
@@ -503,7 +408,7 @@ public class Player{
 		return CardSets;
 	}
 	/**
-	 * Nested loop to determine all possible combinations of sets of Risk cards where the player has one of each card type
+	 * Extracts all combinations of risk cards where there are one of each card type (excluding wilds)
 	 * @param playerCards Current player's cards
 	 * @param infantryCount The count of infantry cards the player is in possession of
 	 * @param cavalryCount The count of cavalry cards the player is in possession of
@@ -548,7 +453,7 @@ public class Player{
 	
 	
 	/**
-	 * Returns all possible subsets of cards with a wildcard
+	 * Returns all possible subsets of cards with a wildcard from a properly organized list of cards 
 	 * @param organizedCards
 	 * @return subsets each subset from the supplied cards as a list
 	 */
@@ -560,7 +465,7 @@ public class Player{
 	}
 	
 	/**
-	 * 
+	 * Organizes cards to be passed to wildSubsets method
 	 * @param playerCards
 	 * @param wildCount
 	 * @return 
