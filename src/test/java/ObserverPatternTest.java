71a82f4b2154a0a428a1ce27e91cb0e1bb12f12a
pasieng <42784814+pasieng@users.noreply.github.com>
2018-10-11 21:06:37 -0500

    Update and rename ObserverPatternTest to ObserverPatternTest.java

--- src/test/java/ObserverPatternTest.java
+++ src/test/java/ObserverPatternTest.java
@@ -9,13 +9,11 @@ import org.junit.Test;
 public class ObserverPatternTest extends TestCase {
 
 	public void test() throws Exception {
-		
 		Player p = new Player();
-    Board b = new Board();
-    p.addObserver(b);
+    		Board b = new Board();
+    		p.addObserver(b);
 		p.setAttackMessage(p, "You have been attacked!");
-    	
 		assertTrue(p.getAttackMessage(), "You have been attacked!");
-    //assertTrue();
+   		//assertTrue();
 	}
 }
