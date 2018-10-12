299403f170a5bfb460f206eb765e068e43adde93
pasieng <42784814+pasieng@users.noreply.github.com>
2018-10-11 20:43:06 -0500

    Observer Pattern Test

--- src/test/java/ObserverPatternTest.java
+++ src/test/java/ObserverPatternTest.java
@@ -1,5 +1,6 @@
 package test.java;
 import junit.framework.TestCase;
+import org.junit.Test;
 import main.java.game.Board;
 import main.java.game.Player;
 import main.java.game.Observer;
@@ -7,10 +8,10 @@ import main.java.game.Observer;
 
 public class ObserverPatternTest extends TestCase {
   Player p = new Player();
-  Board b = new Board();
+  //Board b = new Board();
   
  public void testObserver() throws Exception {
-    	p.addObserver(b);
+    	//p.addObserver(b);
 	p.setAttackMessage("You got attacked!");
     	assertEquals(p.getAttackMessage(), "You got attacked!");
     	//assertEquals(b.getBoardAttackMessage(), "You got attacked!");
