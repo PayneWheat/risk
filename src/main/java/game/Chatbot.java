96a4cc8c8de7ab2ea7f93126e60359aadb68d1e0
Patricia Sieng <42784814+pasieng@users.noreply.github.com>
2018-11-03 13:48:22 -0500

    Create Chatbot.java

---
+++ src/main/java/game/Chatbot.java
@@ -0,0 +1,37 @@
+package main.java.game;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.*;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Chatbot extends TelegramLongPollingBot {
+    @Override
+    
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // TODO
+        return "I_AM_A_BOT";
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return "COSC_4353_PHP";
+    }
+}
