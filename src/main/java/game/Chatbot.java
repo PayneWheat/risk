c04b14c70906beb86bb28445b087e98631cfd610
Patricia Sieng <42784814+pasieng@users.noreply.github.com>
2018-11-04 14:39:40 -0600

    Update Chatbot.java

--- src/main/java/game/Chatbot.java
+++ src/main/java/game/Chatbot.java
@@ -9,20 +9,26 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class Chatbot extends TelegramLongPollingBot {
     @Override
     
-    public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
+  public void onUpdateReceived(Update update) {
+    	// We check if the update has a message and the message has text
+	    //This method (currently) will echo the message sent by the user
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            
+            //Determines content of message
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
             try {
-                execute(message); // Call method to send the message
+                execute(message); // Sending our message object to user
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
     }
-
+    
     @Override
     public String getBotUsername() {
         // TODO
